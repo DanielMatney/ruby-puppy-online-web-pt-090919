881f359ce6f309f23a75e04f5d9c8739f0527536
Daniel Matney <danieljmatney@gmail.com>
2019-10-03 18:25:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,12 @@
 class Dog
   
   @@all = []
+  @@names = []
   
   def initialize(name)
     @name = name
     @@all << self
+    @@names << name
   end
   
   def self.all
@@ -14,6 +16,10 @@ class Dog
   
   def self.clear_all
     @@all = []
+  end
+  
+  def self.print_all
+    @@names
   end
   
 end
